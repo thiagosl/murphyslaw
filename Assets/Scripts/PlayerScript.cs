54b961a93fcd5279b36a3835e99342d3d711dea3
thiagosl <thiagosampaiolima@gmail.com>
2016-03-08 22:26:03 -0300

    Projeto Inicial

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+    private float speed = 3f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+    void FixedUpdate() {
+        Move();
+    }
+
+    void Move() {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        float mov = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(mov*speed, rb.velocity.y);
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
