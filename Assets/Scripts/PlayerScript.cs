48ea22eb53a9eb4a1f59c95691a4e48691f2fb53
thiagosl <thiagosampaiolima@gmail.com>
2016-03-08 23:44:15 -0300

    Adição de pulo

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,27 +1,65 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerScript : MonoBehaviour {
+public class PlayerScript : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
 
-    private float speed = 3f;
+    private bool facingRight;
+    private Rigidbody2D rb;
+    private Transform tf;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-    void FixedUpdate() {
+    // Use this for initialization
+    void Start()
+    {
+        this.speed = 3f;
+        this.jumpForce = 300f;
+        this.facingRight = true;
+        this.rb = GetComponent<Rigidbody2D>();
+        this.tf = GetComponent<Transform>();
+    }
+
+    void FixedUpdate()
+    {
         Move();
+        Jump();
     }
 
-    void Move() {
-        Rigidbody2D rb = GetComponent<Rigidbody2D>();
-        float mov = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(mov*speed, rb.velocity.y);
+    private void Move()
+    {
+        float horizontalMoviment = Input.GetAxis("Horizontal");
+        if (horizontalMoviment < 0 && facingRight)
+        {
+            this.facingRight = false;
+            this.FlipSprite();
+        }
+        else if(horizontalMoviment > 0 && !facingRight)
+        {
+            this.facingRight = true;
+            this.FlipSprite();
+        }
+        rb.velocity = new Vector2(horizontalMoviment * speed, rb.velocity.y);
     }
 
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private void FlipSprite()
+    {
+        Vector3 scale = tf.localScale;
+        scale.x *= -1;
+        tf.localScale = scale;
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown("up") && rb.velocity.y == 0)
+        {
+            this.rb.AddForce(new Vector2(0, jumpForce));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
