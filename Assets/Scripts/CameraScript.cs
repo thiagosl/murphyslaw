facecc93bed65e1cc354053009b5dc26e94334e6
thiagosl <thiagosampaiolima@gmail.com>
2016-03-10 19:55:12 -0300

    Atualização

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+    private Transform playerTransform;
+    private Transform cameraTransform;
+
+	// Use this for initialization
+	void Start () {
+        this.playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        this.cameraTransform = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        cameraTransform.position = new Vector3(playerTransform.position.x, cameraTransform.position.y, cameraTransform.position.z);
+	}
+}
